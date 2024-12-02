6893d5e2f78b6ba963e3c3443a327a96448f432e
Kyllian AUGER <kyllianauger14@gmail.com>
2024-12-02 16:06:32 +0100

    modif Carre.hCommit 2

---
+++ carre.cpp
@@ -0,0 +1,19 @@
+#include "carre.h"
+
+void CCarre::Setsx(int sx1)
+{
+	this->sx = sx1;
+}
+
+void CCarre::Setsy(int sy1)
+{
+}
+
+void CCarre::Setcote(int cote1)
+{
+}
+
+void CCarre::Afficher()
+{
+}
+
--- carre.h
+++ carre.h
@@ -1,1 +1,12 @@
 #pragma once
+
+class CCarre {
+	int  sx;
+	int sy;
+	int cote;
+	void Setsx(int sx1);
+	void Setsy(int sy1);
+	void Setcote(int cote1);
+	void Afficher();
+};
+
